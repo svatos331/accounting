4371834a1091efbc2c36253f0bd44aa0bec3ca26
Zhilin Sviatoslav <svyatoslav.zhilin@synchro.pro>
2025-08-17 23:23:58 +0800

    init host

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ fastify.get('/', async (request, reply) => {
 const start = async () => {
   try {
     const port = process.env.PORT || 3000;
-    await fastify.listen({ port });
+    // Важно: host: '0.0.0.0', чтобы Heroku мог подключаться
+    await fastify.listen({ port, host: '0.0.0.0' });
     console.log(`Server listening on ${port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
 };
+
 start();
