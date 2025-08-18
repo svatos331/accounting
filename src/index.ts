ce2a586c5ce191a1fda5e11b4835a0320f67eeb0
Zhilin Sviatoslav <svyatoslav.zhilin@synchro.pro>
2025-08-18 14:22:05 +0800

    init host

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import app from "./app";
 
 const FASTIFY_PORT = Number(process.env.FASTIFY_PORT) || 3000;
 
-app.listen({ port: FASTIFY_PORT });
+app.listen({ port: FASTIFY_PORT, host: "0.0.0.0" });
